8be93c155930cf79817bab8fdca66b7d16ffcf6e
Manuel ferriz <augusto.ferriz@gmail.com>
2026-03-20 10:31:32 -0600

    fix router

--- main.jsx
+++ main.jsx
@@ -6,8 +6,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/viruta_piso_13">
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
